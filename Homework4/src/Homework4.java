b6acad7cc23129eeab20dfec8ab338bf44e122e7
mrglass <bighill2000@gmail.com>
2015-11-11 01:33:29 -0700

    First commit

---
+++ Homework4/src/Homework4.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/**
+ * Created by mrglass on 11/9/2015.
+ */
+public class Homework4 {
+    static Scanner in  = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int ans = 0;
+        do{
+            System.out.println("__________________________");
+            System.out.println("1: For Sum of Numbers");
+            System.out.println("2: For Pennies for Pay");
+            System.out.println("3: For Celsius to Fahrenheit table");
+            System.out.print("Enter Choice: ");
+            ans = in.nextInt();
+            if(ans == 1)
+                nums();
+            if(ans == 2)
+                pay();
+            if(ans == 3)
+                table();
+        }while(ans != 4);
+    }
+
+    private static void table() {
+        int [] cel = new int[21];
+        double [] fah = new double [21];
+
+        for(int i =0; i < cel.length; i++){
+            cel[i]=i;
+            fah[i] = (9/5)* (double)i + 32;
+        }
+        System.out.printf("%-10s | %-10s\n","Celsius","Fahrenheit");
+        System.out.println("----------------------");
+        for(int i=0; i< cel.length; i++){
+            System.out.printf("%-10s | %-10.2f \n",cel[i],fah[i]);
+        }
+
+    }
+
+    private static void pay() {
+
+    }
+
+    private static void nums() {
+
+    }
+}
